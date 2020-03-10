b202e8ef5d3d51d74c1e862368895c167e9c386e
syedadil2k19 <syed.ahmed1520@gmail.com>
2020-03-11 00:49:25 +0530

    Documentation & clean up

--- Web.API.Domain/Entity/BaseEntity.cs
+++ Web.API.Domain/Entity/BaseEntity.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
 namespace Web.API.Domain.Entity
 {
+    /// <summary>Serves as base for all data models.</summary>
     public class BaseEntity
     {
+        /// <summary>Represents the primary key for data models derived from <see cref="BaseEntity"/></summary>
+        /// <value>Property <c>Id</c> represents an entity's primary key</value>
         [Key]
         public int Id { get; set; }
+        /// <summary>Denotes the date and time of creation of the entity.</summary>
+        /// <value>Timestamp of creation of the entity.</value>
+        /// <remarks>Value must be set manually using approaches like auditing or triggers etc.</remarks>
         public DateTime Created { get; set; }
+        /// <summary>Denotes the date and time of the last modification to the entity.</summary>
+        /// <value>Timestamp of last updation of the entity.</value>
+        /// <remarks>Value must be set manually using approaches like auditing or triggers etc.</remarks>
         public DateTime Modified { get; set; }
     }
 }
--- Web.API.Domain/Entity/User/User.cs
+++ Web.API.Domain/Entity/User/User.cs
@@ -1,17 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Web.API.Domain.Entity.User
 {
+    /// <summary>Data model that represents a user.</summary>
+    /// <seealso cref="Web.API.Domain.Entity.BaseEntity" />
     public class User : BaseEntity
     {
+        /// <summary>  Represents first name of the user.</summary>
+        /// <value>  First name of the user.</value>
         public string FirstName { get; set; }
+        /// <summary>  Represents last name of the user.</summary>
+        /// <value>  Last name of the user.</value>
         public string LastName { get; set; }
+        /// <summary>  Represents middle name of the user.</summary>
+        /// <value>  Middle name of the user.</value>
         public string MiddleName { get; set; }
+        /// <summary>Represents login username for the user.</summary>
+        /// <value>The username for login.</value>
+        /// <remarks>The <c>Username</c> property has a max length of <c>25</c> characters.</remarks>
         [StringLength(25, ErrorMessage = "Username property value length exceeding 25")]
         public string Username { get; set; }
+        /// <summary>  Represents the login password for the user.</summary>
+        /// <value>The password for login.</value>
         public string Password { get; set; }
     }
 }
