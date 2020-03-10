6aadfbdf978dbba63186ab03fb4c13af82585691
syedadil2k19 <syed.ahmed1520@gmail.com>
2020-03-10 19:04:06 +0530

    + Projects - Web.API.Infrastucture, Web.API.Domain

---
+++ Web.API.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Web.API.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Web.API.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Web.API.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
