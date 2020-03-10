3e46bcca7bdebc30aa8cb506d42488a00c443ea1
syedadil2k19 <syed.ahmed1520@gmail.com>
2020-03-10 19:29:07 +0530

    + Domain Models - Entity.BaseEntity, Entity.User.User

---
+++ Web.API.Domain/Entity/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Web.API.Domain.Entity
+{
+    public class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+    }
+}
---
+++ Web.API.Domain/Entity/User/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Web.API.Domain.Entity.User
+{
+    public class User : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        [StringLength(25, ErrorMessage = "Username property value length exceeding 25")]
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
