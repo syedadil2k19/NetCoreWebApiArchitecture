6bd1f4f30104426d22f100719129d7ab216194cf
syedadil2k19 <syed.ahmed1520@gmail.com>
2020-03-11 18:19:29 +0530

    Documentation & clean up | Generate XML Documentation on build

--- Web.API.Infrastructure/ApiContext.cs
+++ Web.API.Infrastructure/ApiContext.cs
@@ -4,10 +4,24 @@ using Web.API.Domain.Entity.User;
 
 namespace Web.API.Infrastructure
 {
+    /// <summary>Serves as the database context class for this API.</summary>
+    /// <seealso cref="Microsoft.EntityFrameworkCore.DbContext" />
     public class ApiContext : DbContext
     {
+        /// <summary>
+        /// <para>
+        /// A public constructor for this database context class that accepts <see cref="DbContextOptions"/>
+        /// and simply passes it to its base class constructor.
+        /// </para>
+        /// NOTE: Required when the database context is set using <see cref="Microsoft.Extensions.DependencyInjection.EntityFrameworkServiceCollectionExtensions.AddDbContext"/> on IServiceCollection
+        /// </summary>
+        /// <param name="options">A <see cref="DbContextOptions"/> object</param>
         public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
 
+        /// <summary>
+        /// <para>Represents entities modelled after <see cref="User"/> data model</para>
+        /// By convention, this creates a database table named "Users"
+        /// </summary>
         public DbSet<User> Users { get; set; }
     }
 }
