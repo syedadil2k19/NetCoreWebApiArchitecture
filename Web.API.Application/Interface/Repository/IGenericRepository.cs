40a9ae96f3f8ac5c7e21979f723457d8e0e2573c
syedadil2k19 <syed.ahmed1520@gmail.com>
2020-03-12 20:01:16 +0530

    + Repository types

---
+++ Web.API.Application/Interface/Repository/IGenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Web.API.Application.Interface.Repository
+{
+    public interface IGenericRepository : IGenericReadOnlyRepository
+    {
+        /// <summary>
+        /// Adds an entity of arbitrary type <typeparamref name="TEntity"/>.
+        /// </summary>
+        /// <param name="entity">An entity of type <typeparamref name="TEntity"/>.</param>
+        /// <typeparam name="TEntity">Type of entity to be added.</typeparam>
+        void Create<TEntity>(TEntity entity) where TEntity : class;
+
+        /// <summary>
+        /// Deletes the entity of arbitrary type <typeparamref name="TEntity"/> that is identified by <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id">The id of the entity to be deleted.</param>
+        /// <typeparam name="TEntity">Type of entity to be deleted.</typeparam>
+        void Delete<TEntity>(object id) where TEntity : class;
+
+        /// <summary>
+        /// Deletes the given <paramref name="entity"/> of arbitrary type <typeparamref name="TEntity"/>.
+        /// </summary>
+        /// <param name="id">An entity of type <typeparamref name="TEntity"/>.</param>
+        /// <typeparam name="TEntity">Type of entity to be deleted.</typeparam>
+        void Delete<TEntity>(TEntity entity) where TEntity : class;
+
+        /// <summary>
+        /// Saves the changes performed on entities in the current scope.
+        /// </summary>
+        void Save();
+
+        /// <summary>
+        /// Saves the changes performed on entities in the current scope, asynchronously.
+        /// </summary>
+        void SaveAsync();
+    }
+}
---
+++ Web.API.Infrastructure/Repository/EntityFrameworkGenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Web.API.Application.Interface.Repository;
+
+namespace Web.API.Infrastructure.Repository
+{
+    public class EntityFrameworkGenericRepository<TContext> : EntityFrameworkGenericReadOnlyRepository<TContext>, IGenericRepository where TContext : DbContext
+    {
+        public EntityFrameworkGenericRepository(TContext mContext) : base(mContext) { }
+
+        public void Create<TEntity>(TEntity entity) where TEntity : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<TEntity>(object id) where TEntity : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<TEntity>(TEntity entity) where TEntity : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
