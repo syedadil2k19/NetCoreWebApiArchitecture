4a760e8b1222090167cff5a02f0de47bd6e048cc
syedadil2k19 <syed.ahmed1520@gmail.com>
2020-03-13 03:05:53 +0530

    Repositories registration for dependency injection

--- Web.API.Infrastructure/DependencyInjection.cs
+++ Web.API.Infrastructure/DependencyInjection.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Web.API.Application.Interface.Repository;
+using Web.API.Infrastructure.Repository;
 
 namespace Web.API.Infrastructure
 {
@@ -15,6 +18,9 @@ namespace Web.API.Infrastructure
             services.AddDbContext<ApiContext>(
                 options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
                 builder => builder.MigrationsAssembly("Web.API")));
+
+            services.TryAdd(ServiceDescriptor.Scoped(typeof(IGenericReadOnlyRepository), typeof(EntityFrameworkGenericReadOnlyRepository<>)));
+            services.TryAdd(ServiceDescriptor.Scoped(typeof(IGenericRepository), typeof(EntityFrameworkGenericRepository<>)));
 
             return services;
         }
